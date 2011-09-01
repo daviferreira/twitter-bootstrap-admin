3fa5abebb1b84e176f3ab36c5a7d8060a780969c
daviferreira <contato@daviferreira.com>
2011-08-31 21:14:00 -0300

    Some refactoring.

--- assets/javascript/admin.js
+++ assets/javascript/admin.js
@@ -1,13 +1,19 @@
 $(function(){
+  $("form:visible:first input:text:first").focus();
+  
   $("#lnk-remember").click(function(e){
     $("#m-login").slideUp("fast", function(){
-      $("#m-remember").slideDown("fast");
+      $("#m-remember").slideDown("fast", function(){
+        $(this).find("input:text:first").focus();
+      });
     });
     e.preventDefault();
   });
   $("#btn-cancel-recovery").click(function(e){
     $("#m-remember").slideUp("fast", function(){
-      $("#m-login").slideDown("fast");
+      $("#m-login").slideDown("fast", function(){
+        $(this).find("input:text:first").focus();
+      });
     });
     e.preventDefault();
   });
