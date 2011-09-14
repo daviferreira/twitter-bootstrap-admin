e4ad887481a3a57be51babeb8a093e7e505bb3a1
Davi Ferreira <contato@daviferreira.com>
2011-09-14 15:12:07 -0300

    All pages set.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,9 +1,9 @@
 var Admin = {
 
   toggleLoginRecovery: function(){
-    var is_login_visible = $('#m-login').is(':visible');
-    (is_login_visible ? $('#m-login') : $('#m-recovery')).slideUp(300, function(){
-      (is_login_visible ? $('#m-recovery') : $('#m-login')).slideDown(300, function(){
+    var is_login_visible = $('#modal-login').is(':visible');
+    (is_login_visible ? $('#modal-login') : $('#modal-recovery')).slideUp(300, function(){
+      (is_login_visible ? $('#modal-recovery') : $('#modal-login')).slideDown(300, function(){
         $(this).find('input:text:first').focus();
       });
     });
